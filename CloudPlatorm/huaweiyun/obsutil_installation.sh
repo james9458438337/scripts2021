42ec75848df46748b4ae04d405667b511557d8d4
Ralph <Ralph@MS188.COM>
2021-10-23 18:13:49 +0800

    update securecrt profile

--- CloudPlatorm/huaweiyun/obsutil_installation.sh
+++ CloudPlatorm/huaweiyun/obsutil_installation.sh
@@ -7,3 +7,7 @@ cd obsutil
 
 ./obsutil config -i=UTPPQVRN3AHPJ4UIPDUF -k=KAC5jwevb4LdrevFxwRms90kFiYisfcvfL1bI3iB -e=obs.ap-southeast-1.myhuaweicloud.com
 
+
+#windows command
+.\obsutil.exe ls obs://haha2021/VASxhy | findstr "20211020"
+
